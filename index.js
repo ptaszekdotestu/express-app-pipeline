18e0df203decded2296d20257d962ea2a7748b19
Aserand <ptaszek114c@o2.pl>
2025-11-23 09:57:03 +0100

    Update do v2

--- index.js
+++ index.js
@@ -1,11 +1,1 @@
-const express = require('express')
-const app = express()
-const port = 3000
-
-app.get('/', (req, res) => {
-  res.send('Hello World - Wersja 1')
-})
-
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+"Hello World - Wersja 2 (Automatyczna!)"
